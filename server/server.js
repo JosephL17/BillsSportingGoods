2e1bb41bcc6dcb93fbe8f3829811a52666c39258
Joseph Lauria <joseph.lauria17@gmail.com>
2025-07-09 15:08:14 -0400

    setup db connection

--- server/server.js
+++ server/server.js
@@ -3,17 +3,22 @@ import { MongoClient, ObjectId } from 'mongodb';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
+dotenv.config();
+const url = process.env.MONGO_DB_URL;
+const dbName = process.env.MONGO_DB;
+const collection = process.env.MONGO_DB_COLLECITON;
+
+
 const app = express();
 const port = 3000;
 app.use(cors()); // Enable CORS for all routes
 // Middleware to parse JSON bodies
 app.use(express.json());
-dotenv.config();
 
 
 app.get('/', (req, res) => {
     res.send("Hello World")
-})
+});
 
 app.listen(port, () => {
     console.log(`Server is running on PORT: ${port}`);
