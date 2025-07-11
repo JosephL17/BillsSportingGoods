0b33759144da0f5b28da69dbbd59b5da3db6330e
Sam Bishara <sbishara12@gmail.com>
2025-07-11 13:46:35 -0400

    Added finished model

--- data/model.py
+++ data/model.py
@@ -32,7 +32,7 @@ df = df.drop(columns=["_id"])
 df.head()
 
 
-# In[33]:
+# In[ ]:
 
 
 # Encode the data so it can be passed to the model
@@ -52,25 +52,51 @@ scaler = StandardScaler()
 x_nums = scaler.fit_transform(df[['price', 'popularity', 'durability']])
 
 # Combine all Encoded data
-x_encoded= hstack([x_vectorized_desc, x_categories.values, x_nums])
+x_encoded = hstack([x_vectorized_desc, x_categories.values, x_nums])
 
 # Fit the model and adjust to find the 3 closest neighbors 
 nbrs = NearestNeighbors(n_neighbors=4, algorithm='auto')
 nbrs.fit(x_encoded)
 
 
-# In[34]:
+# In[37]:
 
 
-# Save the model to disc 
+# Save the model, vectorizer, and scaler to disc 
 
 import pickle
 
 # Specify the file path
 model_file_path = "model.pkl"
+vectorizer_file_path = "vectorizer.pkl"
+scaler_file_path = "scaler.pkl"
 
 with open(model_file_path, 'wb') as file:
     pickle.dump(nbrs, file)
+
+with open(vectorizer_file_path, 'wb') as file:
+    pickle.dump(vectorizer, file)
+
+with open(scaler_file_path, 'wb') as file:
+    pickle.dump(scaler, file)
+
+
+
+# In[38]:
+
+
+# Save missing categories to disc
+import joblib
+cats_file_path = "category_columns.pkl"
+original_cat_cols = list(x_categories.columns)
+joblib.dump(original_cat_cols, cats_file_path)
+
+
+# In[39]:
+
+
+# Save the original training data
+df.to_pickle("training_data.pkl")
 
 
 # In[ ]:
